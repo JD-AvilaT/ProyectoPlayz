f6ae030d930f80de0ea2dbbf8e4371053c69eb65
JD-AvilaT <lolond7110@gmail.com>
2023-03-23 19:16:59 -0500

    feat: components style without responsive

--- app/components/playing/playing.ts
+++ app/components/playing/playing.ts
@@ -17,7 +17,7 @@ class Playing extends HTMLElement{
                 <section>
                     <div class="up">
                         <img src="../app/imgs/avila.jpg">
-                        <input type="text" placeholder="What are you playing">
+                        <input type="text" placeholder="What are you playing?">
                     </div>
                     <button>
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
--- app/components/publicationcard/publicationcard.ts
+++ app/components/publicationcard/publicationcard.ts
@@ -51,17 +51,17 @@ class MyPublications extends HTMLElement {
             if (this.shadowRoot) {
                 this.shadowRoot.innerHTML = `
                 <link rel="stylesheet" href="./app/components/Publicationcard/Publicationcard.css">
-                <section>
-                    <section>
+                <section class="all">
+                    <section class="profile">
                         <img src="${this.imgprofile}">
                         <section>
-                            <h4>${this.name}</h4>
+                            <h3>${this.name}</h3>
                             <p>${this.username}</p>
                         </section>
                     </section>
-                    <p>${this.description}</p>
+                    <p class="description">${this.description}</p>
                     <iframe src="${this.video}"></iframe>
-                    <section>
+                    <section class="likeAppart">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-thumb-up" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3"></path>
