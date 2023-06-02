0de552563b721f14bdc676b6125a11706967bacf
Santiago <santipr2004@gmail.com>
2023-06-02 11:29:38 -0500

    feat: firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,153 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, getDocs, orderBy, query, onSnapshot} from "firebase/firestore";
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, setPersistence, browserSessionPersistence, onAuthStateChanged} from "firebase/auth";
+
+import { Post } from "../types/post";
+import { User } from "../types/users";
+
+import firebaseConfig from "../firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const registerUser = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Promise<boolean> => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential.user);
+      return true;
+    } catch (error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      return false;
+    }
+  };
+
+  const loginUser = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    setPersistence(auth, browserSessionPersistence)
+    .then(() => {
+      return signInWithEmailAndPassword(auth, email, password);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+  }; 
+
+
+const db = getFirestore(app);
+
+const AddPostDB = async (post: Post) =>{
+    try {
+    const where = collection(db, "posts")
+      await addDoc(where,{...post, createdAt: new Date()});
+      return true
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      return false
+    }
+}
+
+const GetPostsDB = async(): Promise<Post[]> =>{
+    const resp: Post[] = [];
+
+    const q=query(collection(db,"posts"), orderBy("createdAt"))
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+      resp.push({
+        ...doc.data()
+      }as Post)
+    });
+    return resp
+}
+
+const GetPostsListener = (cb: (docs: Post[]) => void) => {
+    const q = query(collection(db, "posts"), orderBy("createdAt")); 
+    onSnapshot(q, (collection) => {
+      const docs: Post[] = collection.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Post[];
+      cb(docs);
+    });
+  };
+
+
+  const AddFavoriteDB = async (favorite: Post) =>{
+    try {
+    const where = collection(db, "favorites")
+      await addDoc(where,{...favorite, createdAt: new Date()});
+      return true
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      return false
+    }
+}
+
+const GetFavoritesDB = async(): Promise<Post[]> =>{
+    const resp: Post[] = [];
+
+    const q=query(collection(db,"favorites"), orderBy("createdAt"))
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+      resp.push({
+        ...doc.data()
+      }as Post)
+    });
+    return resp
+}
+
+const GetFavoritesListener = (cb: (docs: Post[]) => void) => {
+    const q = query(collection(db, "favorites"), orderBy("createdAt")); 
+    onSnapshot(q, (collection) => {
+      const docs: Post[] = collection.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Post[];
+      cb(docs);
+    });
+  };
+
+
+const AddUserDB = async (candidate: User) =>{
+    try {
+    const where = collection(db, "users")
+      await addDoc(where,{...candidate, createdAt: new Date()});
+      return true
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      return false
+    }
+}
+
+export default{
+    registerUser,
+    loginUser,
+    AddPostDB,
+    GetPostsDB,
+    GetPostsListener,
+    AddFavoriteDB,
+    GetFavoritesDB,
+    GetFavoritesListener,
+    AddUserDB,
+    onAuthStateChanged,
+}
